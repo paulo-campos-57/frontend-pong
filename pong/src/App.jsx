1ba1f21822056702d9a8e534c5ce78c549789639
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-11-29 17:38:59 -0300

    add: index page
    Co-authored-by: Estela Lacerda <elo@cesar.school>

--- pong/src/App.jsx
+++ pong/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SocketProvider } from './contexts/SocketContext';
+import Index from './pages/Index';
 
 export default function App() {
   return (
     <SocketProvider>
       <Router>
         <Routes>
-          <Route path="/*" element={<div>Pong Game</div>} />
+          <Route path="/" element={<Index />} />
         </Routes>
       </Router>
     </SocketProvider>
---
+++ pong/src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import './style/Index.css';
+
+export default function Index() {
+    return (
+        <>
+            <div className="container">
+                <h1>Bem vindo ao Pong!</h1>
+
+            </div>
+        </>
+    );
+}
